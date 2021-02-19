8ce1fd800ab977de305b6d1648b74b8ed551be06
Joshua <30259508@cityofglacol.ac.uk>
2021-02-19 13:55:43 +0000

    Let there be light!

---
+++ src/components/Team.js
@@ -0,0 +1,279 @@
+import React from 'react'
+import joshuaImage from '../../static/teamphotos/IMG_4401.jpg'
+
+export const Team = () => {
+  return (
+    <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
+      <div className="mx-auto mb-10 lg:max-w-xl sm:text-center">
+        <p className="inline-block px-3 py-px mb-4 text-xs font-semibold tracking-wider text-teal-900 uppercase rounded-full bg-teal-accent-400">
+          The Westray Team
+        </p>
+        <p className="text-base text-gray-700 md:text-lg">
+          Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+          accusantium doloremque laudantium.
+        </p>
+      </div>
+      <div className="grid gap-10 mx-auto lg:grid-cols-2 lg:max-w-screen-lg">
+
+      <div className="grid sm:grid-cols-3">
+          <div className="relative w-full h-48 max-h-full rounded shadow sm:h-auto">
+            <img
+              className="absolute object-cover w-full h-full rounded"
+              src="https://cdn.discordapp.com/attachments/762705846182346832/812312961565589534/IMG_4872.jpeg"
+              alt="Person"
+            />
+          </div>
+          <div className="flex flex-col justify-center mt-5 sm:mt-0 sm:p-5 sm:col-span-2">
+            <p className="text-lg font-bold">Joshua Douglas</p>
+            <p className="mb-4 text-xs text-gray-800">Developer</p>
+            <p className="mb-4 text-sm tracking-wide text-gray-800">
+              Front-end focused developer. Currently learning about blockchain. Lover of architecture, books and miniature dachshunds <span className='text-lg'>🐶</span>
+            </p>
+            <div className="flex items-center space-x-3">
+              <a
+                href="https://github.com/minimalsm"
+                className="text-gray-600 transition-colors duration-300 hover:text-deep-purple-accent-400"
+              >
+                <svg viewBox="0 0 24 24" fill="currentColor" className="h-5">
+                <path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z"/>
+                </svg>
+              </a>
+            </div>
+          </div>
+        </div>
+
+
+        <div className="grid sm:grid-cols-3">
+          <div className="relative w-full h-48 max-h-full rounded shadow sm:h-auto">
+            <img
+              className="absolute object-cover w-full h-full rounded"
+              src="https://cdn.discordapp.com/attachments/812085567584075877/812093732022911006/IMG_3802-2.jpg"
+              alt="Person"
+            />
+          </div>
+          <div className="flex flex-col justify-center mt-5 sm:mt-0 sm:p-5 sm:col-span-2">
+            <p className="text-lg font-bold">Huy Anh Dang</p>
+            <p className="mb-4 text-xs text-gray-800">Developer</p>
+            <p className="mb-4 text-sm tracking-wide text-gray-800">
+              Huy Anh Dang Bio
+            </p>
+            <div className="flex items-center space-x-3">
+              <a
+                href="https://github.com/babylearnstocode"
+                className="text-gray-600 transition-colors duration-300 hover:text-deep-purple-accent-400"
+              >
+                <svg viewBox="0 0 24 24" fill="currentColor" className="h-5">
+                <path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z"/>
+                </svg>
+              </a>
+            </div>
+          </div>
+        </div>
+
+        
+
+        <div className="grid sm:grid-cols-3">
+          <div className="relative w-full h-48 max-h-full rounded shadow sm:h-auto">
+            <img
+              className="absolute object-cover w-full h-full rounded"
+              src="https://cdn.discordapp.com/attachments/762705846182346832/812317516001050644/IMG_3993.JPG"
+              alt="Person"
+            />
+          </div>
+          <div className="flex flex-col justify-center mt-5 sm:mt-0 sm:p-5 sm:col-span-2">
+            <p className="text-lg font-bold">Marie-Terese Völker</p>
+            <p className="mb-4 text-xs text-gray-800">Designer</p>
+            <p className="mb-4 text-sm tracking-wide text-gray-800">
+              Student of Communication Design. Creativity is her passion. Marie-Terese only needs a blank sheet of paper and pen to be happy <span className='text-lg'>😊</span>
+            </p>
+            <div className="flex items-center space-x-3">
+              <a
+                href="https://marieteresevoelkerc5.myportfolio.com/"
+                className="text-gray-600 transition-colors duration-300 hover:text-deep-purple-accent-400"
+              >
+                <svg viewBox="0 0 24 24" fill="currentColor" className="h-5">
+                <path d="M6.188 8.719c.439-.439.926-.801 1.444-1.087 2.887-1.591 6.589-.745 8.445 2.069l-2.246 2.245c-.644-1.469-2.243-2.305-3.834-1.949-.599.134-1.168.433-1.633.898l-4.304 4.306c-1.307 1.307-1.307 3.433 0 4.74 1.307 1.307 3.433 1.307 4.74 0l1.327-1.327c1.207.479 2.501.67 3.779.575l-2.929 2.929c-2.511 2.511-6.582 2.511-9.093 0s-2.511-6.582 0-9.093l4.304-4.306zm6.836-6.836l-2.929 2.929c1.277-.096 2.572.096 3.779.574l1.326-1.326c1.307-1.307 3.433-1.307 4.74 0 1.307 1.307 1.307 3.433 0 4.74l-4.305 4.305c-1.311 1.311-3.44 1.3-4.74 0-.303-.303-.564-.68-.727-1.051l-2.246 2.245c.236.358.481.667.796.982.812.812 1.846 1.417 3.036 1.704 1.542.371 3.194.166 4.613-.617.518-.286 1.005-.648 1.444-1.087l4.304-4.305c2.512-2.511 2.512-6.582.001-9.093-2.511-2.51-6.581-2.51-9.092 0z"/>
+                </svg>
+              </a>
+            </div>
+          </div>
+        </div>
+
+        <div className="grid sm:grid-cols-3">
+          <div className="relative w-full h-48 max-h-full rounded shadow sm:h-auto">
+            <img
+              className="absolute object-cover w-full h-full rounded"
+              src="https://d2lsod3xpjaepj.cloudfront.net/files/assets/2000/1328/crop169/710.jpg"
+              alt="Person"
+            />
+          </div>
+          <div className="flex flex-col justify-center mt-5 sm:mt-0 sm:p-5 sm:col-span-2">
+            <p className="text-lg font-bold">Dieter Rams</p>
+            <p className="mb-4 text-xs text-gray-800">Product Designer</p>
+            <p className="mb-4 text-sm tracking-wide text-gray-800">
+              Some german dude who was apparently pretty influential.
+            </p>
+            <div className="flex items-center space-x-3">
+              <a
+                href="/"
+                className="text-gray-600 transition-colors duration-300 hover:text-deep-purple-accent-400"
+              >
+                <svg viewBox="0 0 24 24" fill="currentColor" className="h-5">
+                <path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z"/>
+                </svg>
+              </a>
+            </div>
+          </div>
+        </div>
+
+
+        <div className="grid sm:grid-cols-3">
+          <div className="relative w-full h-48 max-h-full rounded shadow sm:h-auto">
+            <img
+              className="absolute object-cover w-full h-full rounded"
+              src="https://d2lsod3xpjaepj.cloudfront.net/files/assets/2000/1328/crop169/710.jpg"
+              alt="Person"
+            />
+          </div>
+          <div className="flex flex-col justify-center mt-5 sm:mt-0 sm:p-5 sm:col-span-2">
+            <p className="text-lg font-bold">Dieter Rams</p>
+            <p className="mb-4 text-xs text-gray-800">Product Designer</p>
+            <p className="mb-4 text-sm tracking-wide text-gray-800">
+              Some german dude who was apparently pretty influential.
+            </p>
+            <div className="flex items-center space-x-3">
+              <a
+                href="/"
+                className="text-gray-600 transition-colors duration-300 hover:text-deep-purple-accent-400"
+              >
+                <svg viewBox="0 0 24 24" fill="currentColor" className="h-5">
+                <path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z"/>
+                </svg>
+              </a>
+            </div>
+          </div>
+        </div>
+
+        <div className="grid sm:grid-cols-3">
+          <div className="relative w-full h-48 max-h-full rounded shadow sm:h-auto">
+            <img
+              className="absolute object-cover w-full h-full rounded"
+              src="https://d2lsod3xpjaepj.cloudfront.net/files/assets/2000/1328/crop169/710.jpg"
+              alt="Person"
+            />
+          </div>
+          <div className="flex flex-col justify-center mt-5 sm:mt-0 sm:p-5 sm:col-span-2">
+            <p className="text-lg font-bold">Dieter Rams</p>
+            <p className="mb-4 text-xs text-gray-800">Product Designer</p>
+            <p className="mb-4 text-sm tracking-wide text-gray-800">
+              Some german dude who was apparently pretty influential.
+            </p>
+            <div className="flex items-center space-x-3">
+              <a
+                href="/"
+                className="text-gray-600 transition-colors duration-300 hover:text-deep-purple-accent-400"
+              >
+                <svg viewBox="0 0 24 24" fill="currentColor" className="h-5">
+                <path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z"/>
+                </svg>
+              </a>
+            </div>
+          </div>
+        </div>
+
+        <div className="grid sm:grid-cols-3">
+          <div className="relative w-full h-48 max-h-full rounded shadow sm:h-auto">
+            <img
+              className="absolute object-cover w-full h-full rounded"
+              src="https://d2lsod3xpjaepj.cloudfront.net/files/assets/2000/1328/crop169/710.jpg"
+              alt="Person"
+            />
+          </div>
+          <div className="flex flex-col justify-center mt-5 sm:mt-0 sm:p-5 sm:col-span-2">
+            <p className="text-lg font-bold">Dieter Rams</p>
+            <p className="mb-4 text-xs text-gray-800">Product Designer</p>
+            <p className="mb-4 text-sm tracking-wide text-gray-800">
+              Some german dude who was apparently pretty influential.
+            </p>
+            <div className="flex items-center space-x-3">
+              <a
+                href="/"
+                className="text-gray-600 transition-colors duration-300 hover:text-deep-purple-accent-400"
+              >
+                <svg viewBox="0 0 24 24" fill="currentColor" className="h-5">
+                <path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z"/>
+                </svg>
+              </a>
+            </div>
+          </div>
+        </div>
+
+        <div className="grid sm:grid-cols-3">
+          <div className="relative w-full h-48 max-h-full rounded shadow sm:h-auto">
+            <img
+              className="absolute object-cover w-full h-full rounded"
+              src="https://d2lsod3xpjaepj.cloudfront.net/files/assets/2000/1328/crop169/710.jpg"
+              alt="Person"
+            />
+          </div>
+          <div className="flex flex-col justify-center mt-5 sm:mt-0 sm:p-5 sm:col-span-2">
+            <p className="text-lg font-bold">Dieter Rams</p>
+            <p className="mb-4 text-xs text-gray-800">Product Designer</p>
+            <p className="mb-4 text-sm tracking-wide text-gray-800">
+              Some german dude who was apparently pretty influential.
+            </p>
+            <div className="flex items-center space-x-3">
+              <a
+                href="/"
+                className="text-gray-600 transition-colors duration-300 hover:text-deep-purple-accent-400"
+              >
+                <svg viewBox="0 0 24 24" fill="currentColor" className="h-5">
+                <path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z"/>
+                </svg>
+              </a>
+            </div>
+          </div>
+        </div>
+
+
+        
+        </div>
+        </div>
+        );
+      };
+      
+      
+      // <div className="grid sm:grid-cols-3">
+      //   <div className="relative w-full h-48 max-h-full rounded shadow sm:h-auto">
+      //     <img
+      //       className="absolute object-cover w-full h-full rounded"
+      //       src={joshuaImage}
+      //       alt="Person"
+      //     />
+      //   </div>
+      //   <div className="flex flex-col justify-center mt-5 sm:mt-0 sm:p-5 sm:col-span-2">
+      //     <p className="text-lg font-bold">Joshua Douglas</p>
+      //     <p className="mb-4 text-xs text-gray-800">Developer</p>
+      //     <p className="mb-4 text-sm tracking-wide text-gray-800">
+      //       3rd year Computing Student from Glasgow. Loving front-end development right now. Book Nerd 🤓. Proud lover of Musicals 💃.
+      //     </p>
+      //     <div className="flex items-center space-x-3">
+      //       <a
+      //         href="/"
+      //         className="text-gray-600 transition-colors duration-300 hover:text-deep-purple-accent-400"
+      //       >
+      //         <svg viewBox="0 0 24 24" fill="currentColor" className="h-5">
+      //           <path d="M24,4.6c-0.9,0.4-1.8,0.7-2.8,0.8c1-0.6,1.8-1.6,2.2-2.7c-1,0.6-2,1-3.1,1.2c-0.9-1-2.2-1.6-3.6-1.6 c-2.7,0-4.9,2.2-4.9,4.9c0,0.4,0,0.8,0.1,1.1C7.7,8.1,4.1,6.1,1.7,3.1C1.2,3.9,1,4.7,1,5.6c0,1.7,0.9,3.2,2.2,4.1 C2.4,9.7,1.6,9.5,1,9.1c0,0,0,0,0,0.1c0,2.4,1.7,4.4,3.9,4.8c-0.4,0.1-0.8,0.2-1.3,0.2c-0.3,0-0.6,0-0.9-0.1c0.6,2,2.4,3.4,4.6,3.4 c-1.7,1.3-3.8,2.1-6.1,2.1c-0.4,0-0.8,0-1.2-0.1c2.2,1.4,4.8,2.2,7.5,2.2c9.1,0,14-7.5,14-14c0-0.2,0-0.4,0-0.6 C22.5,6.4,23.3,5.5,24,4.6z" />
+      //         </svg>
+      //       </a>
+      //       <a
+      //         href="/"
+      //         className="text-gray-600 transition-colors duration-300 hover:text-deep-purple-accent-400"
+      //       >
+      //         <svg viewBox="0 0 24 24" fill="currentColor" className="h-5">
+      //           <path d="M22,0H2C0.895,0,0,0.895,0,2v20c0,1.105,0.895,2,2,2h11v-9h-3v-4h3V8.413c0-3.1,1.893-4.788,4.659-4.788 c1.325,0,2.463,0.099,2.795,0.143v3.24l-1.918,0.001c-1.504,0-1.795,0.715-1.795,1.763V11h4.44l-1,4h-3.44v9H22c1.105,0,2-0.895,2-2 V2C24,0.895,23.105,0,22,0z" />
+      //         </svg>
+      //       </a>
+      //     </div>
+      //   </div>
+      // </div>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,10 @@
 import React from "react"
+import { Nav } from "../components/Nav"
+import { Team } from "../components/Team"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return <div className='py4'>
+    <Nav />
+    <Team />
+  </div>
 }
